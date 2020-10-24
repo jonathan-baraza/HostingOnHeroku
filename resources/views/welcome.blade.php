c00630a459fd451ba696e8deef39304af25e1416
Jonathan Baraza <jbaraza46@gmail.com>
2020-10-25 00:07:15 +0300

    corrected welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <x-links/>
     <title>Get started</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <header style="width:100%;border:1px solid black;text-align: center;padding:2%;" class="bg-dark text-light">
@@ -13,6 +14,8 @@
     <br>
 <h4><u>A few pointers before you proceed</u></h4>
 <div style="width: 70%;margin-left: 15%;margin:2%;" class="alert alert-success">The king's Super Blog is an app designed to blogs about anything and share them with friends and family. It allows update features to your blog posts as well as your profile. This is just a beta version of the app.</div>
+<div style="width: 70%;margin-left: 15%;margin:2%;" class="alert alert-success">Use the nav bar to navigate to various functionalities of the app.</div>
+<div style="width: 70%;margin-left: 15%;margin:2%;" class="alert alert-success">Add Posts about anything. You only need a post title and body.</div>
 <div style="width: 70%;margin-left: 15%;margin:2%;" class="alert alert-success">To edit a post click on the post title.</div>
 <div style="width: 70%;margin-left: 15%;margin:2%;" class="alert alert-success">To see all posts by a particular author, click on the author's name.</div>
 <div style="width: 70%;margin-left: 15%;margin:2%;" class="alert alert-danger">Designed and managed by Jonathan Baraza.</div>
